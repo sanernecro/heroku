e66ac1f2ee2196b8f7e9bc097d5610cf74a712e4
Kadir Gün <kadir@kadirgun.com>
2016-10-06 23:24:31 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -2,34 +2,6 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <style type="text/css">
-            body{
-                background: #e9eaed;
-                text-align: center;
-                margin: 0;
-            }
-            ul{
-                list-style: none;
-                margin: 0;
-                padding: 0;
-            }
-            footer{
-                position: fixed;
-                bottom: 0px;
-                width: 100%;
-                height: 20px;
-                background: rgb(<?php echo rand(0,255).",".rand(0,255).",".rand(0,255) ?>);
-            }
-            .<?php echo $randomclass; ?>{
-                    background: rgb(<?php echo rand(0,255).",".rand(0,255).",".rand(0,255) ?>);
-                    width: <?php echo rand(10,250); ?>px;
-                    height: <?php echo rand(10,250); ?>px;
-                    position: absolute;
-                    border-radius: 100%;
-                    top: <?php echo rand(10,250); ?>px;
-                    left: <?php echo rand(10,250); ?>px;
-            }
-        </style>
         <meta property="og:title" content="" />
         <meta property="og:site_name" content="" />
         <meta property="og:description" content="" />
