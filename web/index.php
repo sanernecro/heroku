68640f4d39930cc6214696dec210c65ba0e7120d
Kadir Gün <kadir@kadirgun.com>
2016-11-08 16:15:23 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,6 +9,8 @@
           margin: 0;
         }
 	</style>
+	<meta name="referrer" content="origin" />
+    	<meta name="viewport" content="width=360, initial-scale=1" />
 </head>
 <body>
 <a href="/redirect.php"><svg id="play" style="min-width: 200px; width: 16%; overflow: auto; margin: auto; position: absolute; top: 0; left: 0; bottom: 0; right: 0; opacity: 0.8;" version="1.1" viewBox="0 0 40 40">
