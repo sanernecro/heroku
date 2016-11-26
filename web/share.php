6f91e7d6da174fe980ab129a4a13616bc0013713
Kadir Gün <kadir@kadirgun.com>
2016-11-26 22:41:52 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,16 +1,1 @@
-<?php
-    $host = $_SERVER["HTTP_HOST"];
-    $xsrf = md5($id);
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <meta name="xsrf" content="<?php echo $xsrf; ?>">
-        <meta property="og:url" content="<?php echo "http://".$host."/".$id; ?>.html" />
-        <meta property="og:title" content="<?php echo ucfirst($id); ?>" />
-        <meta property="og:description" content="" />
-        <title><?php echo ucfirst($id); ?></title>
-    </head>
-</html>
+
